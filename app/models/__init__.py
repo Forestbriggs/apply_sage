88af5393103f5906f63a46067b58c234e434afd8
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-08-02 18:45:20 -0700

    created application_statuses and job_categories tables

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,7 @@
 from .db import db
 from .user import User
 from .company import Company
+from .application_status import ApplicationStatus
+from .job_category import JobCategory
+# from .application import Application
 from .db import environment, SCHEMA
