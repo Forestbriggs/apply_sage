72eba8e6126f6503904c8a876c9e1d55fcd2f2f1
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-08-02 17:43:04 -0700

    same as last commit, added changes that were missed

--- app/models/application_status.py
+++ app/models/application_status.py
@@ -1,14 +1,16 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from sqlalchemy.sql import func
 
-class Company(db.Model):
-    __tablename__ = 'companies'
+class ApplicationStatus(db.Model):
+    __tablename__ = 'application_statuses'
     
     if environment == 'production':
         __table_args__ = {'schema': SCHEMA}
         
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    website = db.Column(db.String(2048))
+    application_id = db.Column(db.Integer, nullable=False)
+    status_id = db.Column(db.Integer, nullable=False)
+    status_change_date = db.Column(db.DateTime, nullable=False)
+    notes = db.Column(db.String(1500))
     created_at = db.Column(db.DateTime, default=func.now())
     updated_at = db.Column(db.DateTime, default=func.now())
--- app/models/job_category.py
+++ app/models/job_category.py
@@ -1,14 +1,13 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from sqlalchemy.sql import func
 
-class Company(db.Model):
-    __tablename__ = 'companies'
+class JobCategory(db.Model):
+    __tablename__ = 'job_categories'
     
     if environment == 'production':
         __table_args__ = {'schema': SCHEMA}
         
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    website = db.Column(db.String(2048))
+    name = db.Column(db.String(250), nullable=False)
     created_at = db.Column(db.DateTime, default=func.now())
     updated_at = db.Column(db.DateTime, default=func.now())
