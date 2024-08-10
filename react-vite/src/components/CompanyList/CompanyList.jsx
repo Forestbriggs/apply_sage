e650db1eefa74d0ca46b8deed867d5b1584d0d8e
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-08-09 23:45:54 -0700

    started CompanyList component

--- react-vite/src/components/CompanyList/CompanyList.jsx
+++ react-vite/src/components/CompanyList/CompanyList.jsx
@@ -1,7 +1,41 @@
+import { useDispatch, useSelector } from 'react-redux';
+import LoadingPage from '../LoadingPage';
 import './CompanyList.css';
+import { useEffect, useState } from 'react';
+import { thunkGetUserCompanies } from '../../redux/companies';
 
 export default function CompanyList() {
+    const dispatch = useDispatch();
+    const companies = useSelector(state => state.companies);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        if (!isLoaded) {
+            dispatch(thunkGetUserCompanies()).then(() => {
+                setIsLoaded(true);
+            }, [dispatch, isLoaded])
+        }
+    })
+
     return (
-        'hello'
+        <>
+            {isLoaded &&
+                <div id='company_list'>
+                    <div id='company_list__container'>
+                        <h1>Your Companies</h1>
+                        {companies.allIds.map((company_id) => {
+                            const company = companies.data[company_id];
+                            // TODO create grid layout with companies
+                            return (
+                                <div key={`company-${company_id}`}>
+                                    {company.name}
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            }
+            {!isLoaded && <LoadingPage />}
+        </>
     )
 }
