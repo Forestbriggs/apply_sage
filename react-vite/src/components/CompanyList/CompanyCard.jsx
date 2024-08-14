abfee31af907d9fa5af60e70ca9f518b2d8b00b5
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-08-13 20:02:59 -0700

    added add application functionality to company grid page

--- react-vite/src/components/CompanyList/CompanyCard.jsx
+++ react-vite/src/components/CompanyList/CompanyCard.jsx
@@ -1,4 +1,12 @@
+import { useNavigate } from "react-router-dom"
+
 export default function CompanyCard({ company }) {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        return navigate(`/companies/${company.id}/applications/create`);
+    }
+
     return (
         <div className="company_card">
             <h2>{company.name}</h2>
@@ -8,7 +16,7 @@ export default function CompanyCard({ company }) {
                     <button className='edit_button'>Edit</button>
                     <button className="delete_button">Delete</button>
                 </div>
-                <button className="add_button">Add Application</button>
+                <button onClick={handleClick} className="add_button">Add Application</button>
             </div>
         </div>
     )
